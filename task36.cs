ce596f6017ec954e0f7d7adcea5e46ba2cd5b84d
rtkazan <126402804+rtkazan@users.noreply.github.com>
2023-04-01 12:59:03 +0300

    Add files via upload

---
+++ task36.cs
@@ -0,0 +1,25 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(10, 50);
+}
+
+int sumNumbers(int[] array)
+{
+    int sum = 0;
+    int i = 0;
+    while (i < array.Length)
+    {
+        sum = sum + array[i];
+        i = i + 2;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int [n];
+InputArray(array);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+Console.WriteLine(sumNumbers(array));
